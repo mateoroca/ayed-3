066fdcfc09b5d07b9da2f4076735437371a838fa
mateoroca <mateojoserocaclemntis@gmail.com>
2023-05-09 00:57:12 -0300

    new tp

---
+++ tp-2.3/marco-practico-2.3.py
@@ -0,0 +1,50 @@
+
+
+"""punto 1"""
+""" anchura = int(input("Ingrese la anchura del rectángulo: "))
+altura = int(input("Ingrese la altura del rectángulo: "))
+caracter = input("Ingrese el carácter a utilizar en el dibujo: ")
+
+
+for i in range(altura):
+    for j in range(anchura):
+        print(caracter, end="")
+    print()
+ """
+
+
+"""punto 2"""
+
+""" def es_bisiesto(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+year = int(input("Ingrese un año: "))
+
+if es_bisiesto(year):
+    print(year, "es un año bisiesto.")
+else:
+    print(year, "no es un año bisiesto.") """
+
+"""punto 3"""
+
+""" def Make_list(num) : 
+    list = []
+    for i in range(0,num):
+        word = input("Ingrese una palabra: ")
+        list.append(word)
+    return list
+
+
+num = int ( input("Ingrese un numero :"))
+
+print(Make_list(num)) """
